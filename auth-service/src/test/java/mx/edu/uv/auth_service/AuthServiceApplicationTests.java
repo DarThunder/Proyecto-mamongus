5be7afc0c00307f552a2642ac03c129a61eca168
DarThunder <diazcarazae@gmail.com>
2026-06-09 16:52:38 -0600

    Creación de los servicios Spring Boot

---
+++ auth-service/src/test/java/mx/edu/uv/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.uv.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
